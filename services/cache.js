95c501e43dc8bb0ab3ce0a9fc00d2a3105b8ecf7
austinloveless <austinloveless5171@gmail.com>
2019-01-15 13:37:11 -0700

    added middle ware for caching, and updated readme

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,13 @@ const util = require("util");
 
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "default");
+
   return this;
 };
 
@@ -22,7 +24,7 @@ mongoose.Query.prototype.exec = async function() {
     return exec.apply(this, arguments);
   }
 
-  //Object.sssigncreates a new object by almost
+  //Object.sssigncreates a new object by
   //merging other values into a new object
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
@@ -31,7 +33,7 @@ mongoose.Query.prototype.exec = async function() {
   );
 
   // See if we have a vlaue for 'key' in Redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
   if (cacheValue) {
@@ -49,7 +51,14 @@ mongoose.Query.prototype.exec = async function() {
   const result = await exec.apply(this, arguments);
 
   // 'EX' is for expiration, setting it for 10 seconds
-  client.set(key, JSON.stringify(result), "EX", 10);
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
 
   return result;
+};
+
+module.exports = {
+  //deletes users cached data
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
